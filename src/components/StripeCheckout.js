f21bba1e3f16e587cc1bece3878ab1676ab63b43
Patricio Castro <luckydrawings@gmail.com>
2021-03-23 16:41:02 +0100

    payment card css

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -148,7 +148,7 @@ const Wrapper = styled.section`
   justify-content: center;
   align-content:center;
 form {
-  width: 50vw;
+  width: 80vw;
   align-self: center;
   box-shadow: 0px 0px 0px 0.5px rgba(50, 50, 93, 0.1),
     0px 2px 5px 0px rgba(50, 50, 93, 0.1), 0px 1px 1.5px 0px rgba(0, 0, 0, 0.07);
@@ -195,7 +195,7 @@ input {
 #card-element {
   border-radius: 4px 4px 0 0;
   padding: 12px;
-  border: 1px solid rgba(50, 50, 93, 0.1);
+ 
   max-height: 44px;
   width: 100%;
   background: white;
@@ -313,7 +313,8 @@ button:disabled {
 
 @media only screen and (max-width: 600px) {
   form {
-    width: 80vw;
+    width: 90vw;
+    padding:0;
   }
 }
 
